948e6df534b1065d8dba094bf2e922b0ef3a3a0d
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2024-11-28 07:47:06 +0100

    nested call

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ mod types {
 
 //The calls that are exposed to the world.
 pub enum RuntimeCall {
-	BalancesTransfer { to: types::AccountId, amount: types::Balance },
+	Balances(balances::Call<Runtime>),
 }
 
 
@@ -72,8 +72,8 @@ impl crate::support::Dispatch for Runtime {
 		runtime_call: Self::Call
 	) -> support::DispatchResult {
 		match runtime_call {
-			RuntimeCall::BalancesTransfer { to, amount } => {
-				self.balances.transfer(caller, to, amount)?;
+			RuntimeCall::Balances(call) => {
+				self.balances.dispatch(caller, call)?;
 			},
 		}
 		Ok(())
@@ -100,11 +100,11 @@ fn main() {
 		extrinsics: vec![
 			support::Extrinsic {
 				caller: alice.clone(),
-				call: RuntimeCall::BalancesTransfer { to: bob, amount: 30 }
+				call: RuntimeCall::Balances(balances::Call::Transfer { to: bob, amount: 30 }),
 			},
 			support::Extrinsic {
 				caller: alice,
-				call: RuntimeCall::BalancesTransfer { to: charlie, amount: 20 },
+				call: RuntimeCall::Balances(balances::Call::Transfer { to: charlie, amount: 30 }),
 			},
 		],
 	};
