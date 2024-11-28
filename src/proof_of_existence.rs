8fbfd3955e176bca2223717765d1a7114ab00bb3
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2024-11-28 08:08:25 +0100

    proof of existence functions

--- src/proof_of_existence.rs
+++ src/proof_of_existence.rs
@@ -1,6 +1,8 @@
 use core::fmt::Debug;
 use std::collections::BTreeMap;
 
+use crate::support::DispatchResult;
+
 pub trait Config: crate::system::Config {
     type Content: Debug + Ord;
 }
@@ -15,5 +17,26 @@ impl<T: Config> Pallet<T> {
         Self {
             claims: BTreeMap::new()
         }
+    }
+
+    pub fn get_claim(&self, claim: &T::Content) -> Option<&T::AccountId> {
+        self.claims.get(claim)
+    }
+
+    pub fn create_claim(&mut self, caller: T::AccountId, claim: T::Content) -> DispatchResult {
+        if self.claims.contains_key(&claim) {
+            return Err("this content is already claimed");
+        }
+        self.claims.insert(claim, caller);
+        Ok(())
+    }
+
+    pub fn revoke_claim(&mut self, caller: T::AccountId, claim: T::Content) -> DispatchResult {
+        let owner = self.get_claim(&claim).ok_or("claim does not exist")?;
+        if caller != *owner {
+            return Err("this content is owned by someone else");
+        } 
+        self.claims.remove(&claim);
+        Ok(())
     }
 }
