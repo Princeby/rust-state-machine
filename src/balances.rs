da13ff922ab72aca6f9d98767bdf2ab89109d994
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2025-12-10 13:06:56 +0100

    Updated the transfer

--- src/balances.rs
+++ src/balances.rs
@@ -19,21 +19,23 @@ impl Pallet {
         *self.balances.get(who).unwrap_or(&0)
     }
 
-    pub fn transfer(&mut self, from: &String, to: &String, amount: u128) -> Result<(), &'static str>{
-        let from_balance = self.balance(&from.to_string());
-        let new_from_balance = from_balance.checked_sub(amount).ok_or("Not enough funds.")?;
-        let to_balance = self.balance(to);
-        let new_to_balance = to_balance
-            .checked_add(amount)
-            .ok_or("Overflow adding to balance")?;
+    pub fn transfer(
+		&mut self,
+		caller: String,
+		to: String,
+		amount: u128,
+	) -> Result<(), &'static str> {
+		let caller_balance = self.balance(&caller);
+		let to_balance = self.balance(&to);
 
-        // update storage
-        self.set_balance(from, new_from_balance);
-        self.set_balance(to, new_to_balance);
+		let new_caller_balance = caller_balance.checked_sub(amount).ok_or("Not enough funds.")?;
+		let new_to_balance = to_balance.checked_add(amount).ok_or("Overflow")?;
 
-        Ok(())
+		self.balances.insert(caller, new_caller_balance);
+		self.balances.insert(to, new_to_balance);
 
-    }
+		Ok(())
+	}
 
 
 }
@@ -52,22 +54,23 @@ mod tests {
     }
 
     #[test]
-    fn transfer() {
-        let mut balances = super::Pallet::new();
+    #[test]
+	fn transfer_balance() {
+		let mut balances = super::Pallet::new();
 
-        assert_eq!(balances.balance(&"alice".to_string()), 0);
-        balances.set_balance(&"alice".to_string(), 100);
-        balances.set_balance(&"bob".to_string(), 10);
+		assert_eq!(
+			balances.transfer("alice".to_string(), "bob".to_string(), 51),
+			Err("Not enough funds.")
+		);
 
-        assert_eq!(balances.balance(&"alice".to_string()), 100);
-        assert_eq!(balances.balance(&"bob".to_string()), 10);
+		balances.set_balance(&"alice".to_string(), 100);
+		assert_eq!(balances.transfer("alice".to_string(), "bob".to_string(), 51), Ok(()));
+		assert_eq!(balances.balance(&"alice".to_string()), 49);
+		assert_eq!(balances.balance(&"bob".to_string()), 51);
 
-        balances.transfer(&"alice".to_string(), &"bob".to_string(), 50);
-        assert_eq!(balances.balance(&"alice".to_string()), 50);
-        assert_eq!(balances.balance(&"bob".to_string()), 60);
-
-
-
-
-    }
+		assert_eq!(
+			balances.transfer("alice".to_string(), "bob".to_string(), 51),
+			Err("Not enough funds.")
+		);
+	}
 }
