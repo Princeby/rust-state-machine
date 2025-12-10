332330a571922c808dd38c0d33a5dc7387259ffe
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2025-12-10 12:59:51 +0100

    added a transfer api

--- src/balances.rs
+++ src/balances.rs
@@ -19,6 +19,22 @@ impl Pallet {
         *self.balances.get(who).unwrap_or(&0)
     }
 
+    pub fn transfer(&mut self, from: &String, to: &String, amount: u128) -> Result<(), &'static str>{
+        let from_balance = self.balance(&from.to_string());
+        let new_from_balance = from_balance.checked_sub(amount).ok_or("Not enough funds.")?;
+        let to_balance = self.balance(to);
+        let new_to_balance = to_balance
+            .checked_add(amount)
+            .ok_or("Overflow adding to balance")?;
+
+        // update storage
+        self.set_balance(from, new_from_balance);
+        self.set_balance(to, new_to_balance);
+
+        Ok(())
+
+    }
+
 
 }
 
@@ -32,6 +48,26 @@ mod tests {
         balances.set_balance(&"alice".to_string(), 100);
         assert_eq!(balances.balance(&"alice".to_string()), 100);
         assert_eq!(balances.balance(&"bob".to_string()), 0);
+
+    }
+
+    #[test]
+    fn transfer() {
+        let mut balances = super::Pallet::new();
+
+        assert_eq!(balances.balance(&"alice".to_string()), 0);
+        balances.set_balance(&"alice".to_string(), 100);
+        balances.set_balance(&"bob".to_string(), 10);
+
+        assert_eq!(balances.balance(&"alice".to_string()), 100);
+        assert_eq!(balances.balance(&"bob".to_string()), 10);
+
+        balances.transfer(&"alice".to_string(), &"bob".to_string(), 50);
+        assert_eq!(balances.balance(&"alice".to_string()), 50);
+        assert_eq!(balances.balance(&"bob".to_string()), 60);
+
+
+
 
     }
 }
