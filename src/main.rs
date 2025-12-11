6cde775ec53503c7240dd69e841ad6fde9fbff4e
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2025-12-11 02:01:31 +0100

    Created runtime

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 mod balances;
 mod system;
 
+
+pub struct Runtime {
+    system: system::Pallet,
+    balances: balances::Pallet,
+}
+
+
+impl Runtime {
+    fn new() -> Self { 
+        Self {
+            system: system::Pallet::new(),
+            balances: balances::Pallet::new()
+        }
+    }
+}
+
 fn main() {
 	println!("Hello, world!");
 
