b005cc205d7cf1c1ff7e85ed186bab17c12e7c8c
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2025-12-13 11:25:20 +0100

    Enabled full end dispatch logic to run

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ mod types {
 }
 
 pub enum RuntimeCall {
-    BalancesTransfer { to: types::AccountId, amount: types::Balance },
+    Balances(balances::Call<Runtime>),
 }
 
 #[derive(Debug)]
@@ -71,8 +71,8 @@ impl crate::support::Dispatch for Runtime {
     fn dispatch(&mut self, caller: Self::Caller, runtime_call: Self::Call) -> support::DispatchResult {
         
         match runtime_call {
-            RuntimeCall::BalancesTransfer { to, amount } => {
-                self.balances.transfer(caller, to, amount)?;
+            RuntimeCall::Balances(call) => {
+                self.balances.dispatch(caller, call)?;
             },
         }
         Ok(())
@@ -94,11 +94,11 @@ fn main() {
         extrinsics: vec![
             support::Extrinsic {
                 caller: alice.clone(),
-                call: RuntimeCall::BalancesTransfer { to: bob, amount: 30 },
+                call: RuntimeCall::Balances(balances::Call::Transfer{ to: bob, amount: 30}),
             },
             support::Extrinsic {
                 caller: alice,
-                call: RuntimeCall::BalancesTransfer { to: charlie, amount: 20 },
+                call: RuntimeCall::Balances(balances::Call::Transfer{to: charlie, amount: 20}),
             },
         ],
     };
