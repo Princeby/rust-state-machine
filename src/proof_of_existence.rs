8464e50ef5213f3e13c4648eb8b2d0d63f13c0b7
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2025-12-14 13:51:30 +0100

    Added the cargo expand to see the code generated by using expand

--- src/proof_of_existence.rs
+++ src/proof_of_existence.rs
@@ -22,6 +22,10 @@ impl<T: Config> Pallet<T> {
     pub fn get_claim(&self, claim: &T::Content) -> Option<&T::AccountId> {
         self.claims.get(claim)
     }
+}
+
+#[macros::call]
+impl<T: Config> Pallet<T> {
 
     pub fn create_claim(&mut self, caller: T::AccountId, claim: T::Content) -> DispatchResult {
 		if self.claims.contains_key(&claim) {
@@ -42,27 +46,6 @@ impl<T: Config> Pallet<T> {
 
 }
 
-pub enum Call<T: Config> {
-    CreateClaim { claim: T::Content },
-    RevokeClaim { claim: T::Content },
-}
-
-impl<T: Config> crate::support::Dispatch for Pallet<T> {
-    type Caller = T::AccountId;
-    type Call = Call<T>;
-
-    fn dispatch(&mut self, caller: Self::Caller, call: Self::Call) -> crate::support::DispatchResult {
-        match call {
-            Call::CreateClaim { claim } => {
-                self.create_claim(caller, claim)?;
-            },
-            Call::RevokeClaim { claim } => {
-                self.revoke_claim(caller, claim)?;
-            }
-        }
-        Ok(())
-    }
-}
 
 
 #[cfg(test)]
