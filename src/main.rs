34dba31ae1f70781deee72c78b66ca4709742a7c
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2025-12-14 13:37:22 +0100

    Replaced the call methods with macros

--- src/main.rs
+++ src/main.rs
@@ -106,14 +106,14 @@ fn main() {
         extrinsics: vec![
             support::Extrinsic {
                 caller: alice.clone(),
-                call: RuntimeCall::Balances(balances::Call::Transfer{ 
+                call: RuntimeCall::Balances(balances::Call::transfer{ 
                     to: bob.clone(), 
                     amount: 30
                 }),
             },
             support::Extrinsic {
                 caller: alice.clone(),
-                call: RuntimeCall::Balances(balances::Call::Transfer{to: charlie, amount: 20}),
+                call: RuntimeCall::Balances(balances::Call::transfer{to: charlie, amount: 20}),
             },
         ],
     };
